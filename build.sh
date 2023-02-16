942ae21ccc37ee183f375590d6b339d766ce2638
serv <liuyuhua69@gmail.com>
2023-02-16 13:08:02 +0800

    增加变异sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd images_rc
+sh build.sh
+cd ..
+cd ui
+sh build.sh
+cd ..
--- ui/build.sh
+++ ui/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo '开始编译ui文件...'
 pyside6-uic login.ui -o ui_login.py
 pyside6-uic home.ui -o ui_home.py
+echo 'ui文件编译完成!'
