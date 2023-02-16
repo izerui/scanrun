ffd85fd26ef6ef1ae564e73fa22ca807c95c35f5
serv <liuyuhua69@gmail.com>
2023-02-16 18:27:18 +0800

    1

--- home_window.py
+++ home_window.py
@@ -22,23 +22,22 @@ class HomeWindow(QMainWindow):
         QMessageBox.information(None, '提示', '深圳云集智造系统有限公司')
 
     def loadOrders(self):
-        def callback(result):
-            data = json.loads(result.text)['data']
-            self.ui.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-            self.ui.tableWidget.setRowCount(len(data['content']))
-            i = 0
-            for d in data['content']:
-                self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(d['orderDocNo']))
-                self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(d['customer']['customerName']))
-                self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(d['customerOrderDocNo']))
-                self.ui.tableWidget.setItem(i, 3, QTableWidgetItem(d['totalOrderAmount']))
-                self.ui.tableWidget.setItem(i, 4, QTableWidgetItem(d['employeeName']))
-                self.ui.tableWidget.setItem(i, 5, QTableWidgetItem(d['createTime']))
-                i += 1
-            self.ui.tableWidget.show()
         reqParam = {"docStatus": "DRAFT", "pageIndex": 0, "pageSize": 20, "total": 0}
-        NetUtil.post('https://yj2025.com/ierp/sale-pc/v1/sale/order/list', json=reqParam,
-                           postCode='M1018', callback=callback)
+        result = NetUtil.post('https://yj2025.com/ierp/sale-pc/v1/sale/order/list', json=reqParam,
+                           postCode='M1018')
+        data = json.loads(result.text)['data']
+        self.ui.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.ui.tableWidget.setRowCount(len(data['content']))
+        i = 0
+        for d in data['content']:
+            self.ui.tableWidget.setItem(i, 0, QTableWidgetItem(d['orderDocNo']))
+            self.ui.tableWidget.setItem(i, 1, QTableWidgetItem(d['customer']['customerName']))
+            self.ui.tableWidget.setItem(i, 2, QTableWidgetItem(d['customerOrderDocNo']))
+            self.ui.tableWidget.setItem(i, 3, QTableWidgetItem(d['totalOrderAmount']))
+            self.ui.tableWidget.setItem(i, 4, QTableWidgetItem(d['employeeName']))
+            self.ui.tableWidget.setItem(i, 5, QTableWidgetItem(d['createTime']))
+            i += 1
+        # self.ui.tableWidget.show()
 
     def logout(self):
         self.loginExistSignal.emit('logout')
