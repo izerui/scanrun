48df8577d2b4f6efb2fd678b8bed4856417f9884
serv <yifeng2000@126.com>
2023-02-17 00:26:55 +0800

    增加类型声明

--- thread_executor.py
+++ thread_executor.py
@@ -1,8 +1,13 @@
+from typing import Callable
+
+from PySide6.QtCore import QThread
+
+
 class ThreadExecutor(object):
     def __init__(self):
         super().__init__()
 
-    def execute_new_thread(self, threadName, newThread, signalName=None, connectFun=None):
+    def execute_new_thread(self, threadName:str, newThread:QThread, signalName:str=None, connectFun:Callable=None):
         if hasattr(self, threadName) and getattr(self, threadName).isRunning():
             pass
         else:
