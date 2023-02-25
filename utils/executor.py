0f0a29e3f115cc746674e6593cd8069720d4256f
serv <liuyuhua69@gmail.com>
2023-02-25 21:15:44 +0800

    /Users/liuyuhua/PycharmProjects/scanrun/

--- utils/executor.py
+++ utils/executor.py
@@ -146,4 +146,4 @@ class SoundThread(QThread):
         self.soundFile = soundFile
 
     def run(self) -> None:
-        playsound(f'{sys.path[0]}/{self.soundFile}', block=False)
+        playsound(f'{sys.path[0]}{self.soundFile}', block=False)
