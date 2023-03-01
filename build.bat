8b91921bc582c0e824ce7d21cb614204245329d2
serv <liuyuhua69@gmail.com>
2023-03-01 10:52:45 +0800

    任务查询

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 rmdir /s /q build
 :: https://pyinstaller.org/en/stable/usage.html
-:: .\venv\Scripts\pyinstaller -n scanrun --windowed main.py
-.\venv\Scripts\pyinstaller scanrun.spec
+.\venv\Scripts\pyinstaller -n scanrun --windowed main.py
--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf build dist
 # https://pyinstaller.org/en/stable/usage.html
-# pyinstaller -n scanrun -w -i resources/Icon.icns main.py
-pyinstaller scanrun.spec
+ pyinstaller -n scanrun -w -i resources/Icon.icns main.py
