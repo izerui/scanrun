6e2f57b758b31868bed091e9b1c03b21376c54bd
serv <liuyuhua69@gmail.com>
2023-03-01 20:41:39 +0800

    组件化

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rmdir /s /q build dist
 :: https://pyinstaller.org/en/stable/usage.html
-.\venv\Scripts\pyinstaller -n scanrun --windowed main.py
+.\venv\Scripts\pyinstaller -n scanrun --windowed -i resources\Icon.ico main.py
