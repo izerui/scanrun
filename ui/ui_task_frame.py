c88a83d3dac22c6923639777aab8176d0883224f
serv <liuyuhua69@gmail.com>
2023-03-03 15:54:11 +0800

    mac 版本 的pyside6

--- ui/ui_task_frame.py
+++ ui/ui_task_frame.py
@@ -3,17 +3,24 @@
 ################################################################################
 ## Form generated from reading UI file 'task_frame.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.1.3
+## Created by: Qt User Interface Compiler version 6.4.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *  # type: ignore
-from PySide6.QtGui import *  # type: ignore
-from PySide6.QtWidgets import *  # type: ignore
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFormLayout, QGridLayout,
+    QHeaderView, QLabel, QLineEdit, QPushButton,
+    QScrollArea, QSizePolicy, QSplitter, QTableView,
+    QVBoxLayout, QWidget)
 
 from controller.component import PagingWidget
-
 import images_rc
 
 class Ui_TaskFrame(object):
