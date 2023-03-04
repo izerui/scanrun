258e4c18bcf82bed532ec885aa2023e3245d4765
serv <liuyuhua69@gmail.com>
2023-03-04 18:02:18 +0800

    恢复使用原始的直接播放

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 # -*- coding: UTF-8 -*-
-
-# This is a sample Python script.
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-import sys
+import logging
 
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QWidget
+from shiboken6 import *
 
 from controller.home import HomeWindow
 from controller.login import LoginWindow
 from utils.context import Context
-from shiboken6 import *
+
+
+# This is a sample Python script.
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
 class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.initDefaultSettings()
+        self.init()
 
     # 槽函数：显示登录页
     @Slot()
@@ -37,11 +36,18 @@ class MainWindow(QWidget):
         self.home.loginExistSignal.connect(self.loadLoginWindow)
         self.home.show()
 
-    def initDefaultSettings(self):
+    def init(self):
         Context.setDefaultSettings('gateway/domain', 'https://yj2025.com')
         Context.setDefaultSettings('scan/auto_code', False)
         Context.setDefaultSettings('login/username', '')
         Context.setDefaultSettings('login/password', '')
+
+        LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+        isDebug = True if sys.gettrace() else False
+        if isDebug:
+            logging.basicConfig(format=LOG_FORMAT)
+        else:
+            logging.basicConfig(filename='error.log', level=logging.ERROR, format=LOG_FORMAT)
 
 
 if __name__ == '__main__':
