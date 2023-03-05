f44ee7a08cffec39e459cd9803fe0b41aab76ef3
serv <liuyuhua69@gmail.com>
2023-03-05 13:51:15 +0800

    --uac-admin

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rmdir /s /q build dist
 :: https://pyinstaller.org/en/stable/usage.html
-.\venv\Scripts\pyinstaller -n scanrun --add-data="media;media" --windowed -i resources\Icon.ico main.py
+.\venv\Scripts\pyinstaller -n scanrun --uac-admin --add-data="media;media" --windowed -i resources\Icon.ico main.py
